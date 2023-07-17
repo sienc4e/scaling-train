aab506b67cd2e33c04ff3065c609b2f009b3578e
sienc4e <kostyalazkov2@gmail.com>
2023-07-17 18:39:09 +0600

    third commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const header = document.querySelector("#header");
+const toggleClass = "header-scrolled";
+
+window.addEventListener("scroll", () => {
+    const currentScroll = window.pageYOffset;
+    if (currentScroll > 150) {
+        header.classList.add(toggleClass);
+    } else {
+        header.classList.remove(toggleClass);
+    }
+});
